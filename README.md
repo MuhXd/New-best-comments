5ea8d2a7d2afda6a4556764939138da808363840
MuhXd <momo2010sofia@gmail.com>
2024-02-24 22:09:10 +0000

    sdfsfdsf

---
+++ README.md
@@ -0,0 +1,3 @@
+# new best
+
+* Read me?
---
+++ src/main.cpp
@@ -0,0 +1,59 @@
+#include <Geode/Geode.hpp>
+
+using namespace geode::prelude;
+
+#include <Geode/modify/PlayerObject.hpp>
+#include <Geode/binding/PlayerObject.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+int per =0;
+int Best = 0;
+auto name = "test";
+void uploadNewBest() {
+   auto manager = GameLevelManager::sharedState();
+   manager->uploadAccountComment("New Best on " << (levelname) << " and got " << (Best) << " %");
+   name = "";
+   Best = 0;
+}
+void setvalue() {
+	if (Best < per) {
+	Best = per;
+	name = "";
+	}
+}
+class $modify(PlayerObject){
+	void playerDestroyed(bool p0){
+
+		PlayerObject::playerDestroyed(p0);
+
+		setvalue()
+		
+	}
+};
+
+
+class $modify(PlayLayer) {
+	void levelComplete() {
+		PlayLayer::levelComplete();
+		setvalue();
+	}
+	void onExit() {
+		uploadNewBest();
+	}
+    void updateProgressbar() {
+        PlayLayer::updateProgressbar();
+            if (!m_level->isPlatformer()) {
+                if ( m_isPracticeMode) {
+                    return;
+                }
+               
+                float percent = static_cast<float>(PlayLayer::getCurrentPercentInt());
+                
+            
+                if (percent > (m_level->m_normalPercent)) {
+                    per = percent
+					name = m_level->m_levelName
+                } 
+            }
+	}
+
+};
